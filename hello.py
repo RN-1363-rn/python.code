01b5c6c9e614df7c01051780b7ee002240b117c7
ASUS <h.soleimani6131@yahoo.com>
2026-01-15 12:08:34 +0200

    second

--- hello.py
+++ hello.py
@@ -1,4 +1,29 @@
+from sklearn.datasets import make_low_rank_matrix
+
 print('hello world')
 print('hello world2')
 print("reihaneh")
 print("hello friend")
+var1 = 100
+var2 = 2.10
+var3 = "akram"
+var4 = (1,2,3,4)
+var5 = [1,2,3,4,5]
+var6 = {"akram": 123,"rodi":222}
+var7 = 4+2j
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+print(var7)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
+print(type(var6))
+print(type(var7))
+user = input("Enter your name: ")
+print("nice to meet you  "  + user +"!")
